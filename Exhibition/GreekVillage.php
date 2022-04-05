379be62ab07ef93dce20d1588d4a8651b4a4df73
rlarbqor00 <rlarbqor00@naver.com>
2022-04-05 18:37:04 +0900

    0405.2

--- Exhibition/GreekVillage.php
+++ Exhibition/GreekVillage.php
@@ -6,34 +6,79 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>그리스신화박물관</title>
-    <link rel="stylesheet" href="../css/common.css">
-    <link rel="stylesheet" href="../css/subpage_common.css">
-    <link rel="stylesheet" href="../css/about.css">
+    <link rel="stylesheet" href="../css/common.css?<?php echo date('Y h:i:s'); ?>">
+    <link rel="stylesheet" href="../css/subpage_common.css?<?php echo date('Y h:i:s'); ?>">
+    <link rel="stylesheet" href="../css/about.css?<?php echo date('Y h:i:s'); ?>">
     <script src="//code.jquery.com/jquery-3.4.1.min.js"></script>
 
 </head>
 
 <body>
     <div id="wrap">
+        <!-- header -->
         <header>
             <?php include("../header.html"); ?>
         </header>
-        <section id="slider">
-            <img src="../img/aboutus/sub_visual_bg.jpg" alt="">
-        </section>
+        <!-- header end -->
+
+        <!-- sub_visual -->
+        <div id="sub_visual"
+            style="background:url(../img/exhibition/sub_visual_bg.jpeg) no-repeat 50% top; background-size:auto 100%;">
+            <div class="text">
+                <img src="../img/exhibition/sub_visual_txt.png" alt="그리스 신화가 살아 숨쉬는 곳  Greek mythology is alive">
+            </div>
+        </div>
+        <!-- sub_visual end -->
+
+        <!-- sub -->
         <div id="sub_main">
+            <!-- left menu -->
             <aside id="left_menu">
                 <?php include("left_menu.html"); ?>
             </aside>
+            <!-- left menu end -->
+
+            <!-- location -->
             <section id="sub_contents">
-                <h3>그리스마을<span>HOME>전시안내>그리스마을</span></h3>
-                <h1></h1>
-                <article id="sub_contact">
-                    <div>
-                        <h5>관람문의<span>Tel : 064-773-5800</span>관람시간<span>09:00~18:00 (입장마감 17:00)</span></h5>
-                    </div>
-                </article>
+                <h3>그리스마을<span>HOME>전시안내>그리스 마을</span></h3>
             </section>
+            <!-- location end -->
+
+            <div id="contents">
+                <!-- 페이지 내용 시작 -->
+                <div id="txt_box_color">
+                    <div id="txt_box_color_wrap">
+                        <p>
+                            <span class="title">
+                                <img src="/img/exhibition/exhibition_title01.gif" alt="창조관">
+                            </span>
+                            <strong class="blue">“두근두근 콩닥콩닥! 이 세상은 어떻게 시작되었을까요?”</strong>
+                            <br>
+                            세상에 아무것도 없었던 때, 그저 끝없이 세상이 펼쳐져 있던 때, 이 상태를 그리스인들은 카오스(혼돈)라고 불렀습니다.
+                            <br>
+
+                            대지의 여신 가이아와 하늘의 신 우라노스의 천지창조, 이어진 크로노스의 시대와 제우스의 탄생 그리고 전쟁,
+                            <br>
+
+                            올림포스 12신이 세상을 통치하게 되기까지의 과정을 아름다운 월 그래픽으로 만나보세요!
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="exhibition_img">
+                    <img src="/img/exhibition/exhibition_img01.jpeg" alt="창조관 전경">
+                </div>
+                <!-- 페이지 내용 끝 -->
+
+            </div>
+            <h1></h1>
+            <article id="sub_contact">
+                <div>
+                    <h5>관람문의<span>Tel : 064-773-5800</span>관람시간<span>09:00~18:00 (입장마감 17:00)</span></h5>
+                </div>
+            </article>
+            <!-- </section> -->
         </div>
         <footer>
             <?php include("../footer.html"); ?>
