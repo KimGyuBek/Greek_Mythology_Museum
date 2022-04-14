cca3214ccf8a5421ae0354248616b4bd675957f6
rlarbqor00 <rlarbqor00@naver.com>
2022-04-14 19:08:06 +0900

    add login

--- Login/logout.php
+++ Login/logout.php
@@ -1,10 +1,24 @@
-<?php
-session_start();
-unset($_SESSION['username']);
-unset($_SESSION['userid']);
-unset($_SESSION['userpasswd']);
+<!DOCTYPE html>
+<html lang="en">
 
-echo "
-<script>
-loaction.href='../index.php'
-<?script> ";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    session_destroy();
+    ?>
+
+    <script>
+    alert("로그아웃 되었습니다.");
+    location.replace('../index.php');
+    </script>
+
+</body>
+
+</html>
